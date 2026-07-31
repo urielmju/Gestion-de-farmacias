3b97ca3bb98f8240281511aefc10aeea7c38e8d7
urielmorganbrown@gmail.com <urielmorganbrown@gmail.com>
2026-07-30 21:10:40 -0600

    Aplicar configuracion de produccion aprendida del proyecto de Hospital
    machineKey fija (evita fallos de token anti-falsificacion tras
    reciclos del proceso en Azure), Configuration=Release en el workflow
    de deploy, y log de errores no manejados en LogFiles/App_Data para
    diagnostico sin exponer detalles al publico.

--- PIV_PF_ProyectoFinal/Global.asax.cs
+++ PIV_PF_ProyectoFinal/Global.asax.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,42 @@ namespace PIV_PF_ProyectoFinal
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+            if (ex == null)
+                return;
+
+            var texto = string.Format(
+                "{0:yyyy-MM-dd HH:mm:ss} UTC{1}URL: {2}{1}{3}{1}{4}{1}",
+                DateTime.UtcNow, Environment.NewLine,
+                Request?.Url, ex.ToString(),
+                new string('-', 60));
+
+            var home = Environment.GetEnvironmentVariable("HOME");
+            if (!string.IsNullOrEmpty(home))
+            {
+                try
+                {
+                    var ruta = Path.Combine(home, "LogFiles", "error_log.txt");
+                    File.AppendAllText(ruta, texto + Environment.NewLine);
+                    return;
+                }
+                catch
+                {
+                }
+            }
+
+            try
+            {
+                var ruta = Server.MapPath("~/App_Data/error_log.txt");
+                File.AppendAllText(ruta, texto + Environment.NewLine);
+            }
+            catch
+            {
+            }
         }
     }
 }
